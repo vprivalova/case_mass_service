d47452c4e332eb9899445f1b6e77fd4e76f8987a
Anastasia030 <a.nizovtseva@g.nsu.ru>
2024-02-25 23:31:37 +0700

    commit_2

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 prices = {'АИ-80': 32, 'АИ-92': 49, 'АИ-95': 52, 'АИ-98': 65}
+
+with open('initial data.txt', 'r', encoding='utf-8') as f_data:
+    count_station = 0
+    stations = {}
+    for lines in f_data:
+        line = input().split()
+        station
+        count_station += 1
+
+
+
+with open('input.txt', 'r') as f_:
+
+
+colonka_1 = {'place': , 'AU-80': , 'car': }
+
+
