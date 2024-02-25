416ac57b32b5c37ac77bda4c111a312510d7d79e
vprivalova <privalovaa.vv@gmail.com>
2024-02-25 23:43:01 +0700

    COMMIT 3

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 prices = {'АИ-80': 32, 'АИ-92': 49, 'АИ-95': 52, 'АИ-98': 65}
 
-with open('initial data.txt', 'r', encoding='utf-8') as f_data:
-    count_station = 0
-    stations = {}
-    for lines in f_data:
-        line = input().split()
-        station
-        count_station += 1
 
+with open('initial_data.txt', 'r', encoding='utf-8') as f_data:
+    stations = []
+    for line in f_data:
+        line = line.rstrip()
+        line = line.split(' ')
+        station = {'c_number': line[0], 'max_queue': line[1], 'petrol_types': line[2:]}
+        stations.append(station)
 
+print(stations)
 
-with open('input.txt', 'r') as f_:
-
-
-colonka_1 = {'place': , 'AU-80': , 'car': }
-
+#with open('input.txt', 'r') as f_:
 
