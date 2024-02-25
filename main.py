402139f991951041dbb493902d4daaca5d78ef9c
vprivalova <privalovaa.vv@gmail.com>
2024-02-25 23:45:55 +0700

    COMMIT 4

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ with open('initial_data.txt', 'r', encoding='utf-8') as f_data:
     for line in f_data:
         line = line.rstrip()
         line = line.split(' ')
-        station = {'c_number': line[0], 'max_queue': line[1], 'petrol_types': line[2:]}
+        station = {'c_number': int(line[0]), 'max_queue': int(line[1]), 'petrol_types': line[2:], 'current_queue': 0}
         stations.append(station)
 
 print(stations)
 
-#with open('input.txt', 'r') as f_:
-
+with open('input.txt', 'r') as f_cars:
+    pass
